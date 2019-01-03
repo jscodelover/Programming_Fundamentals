154dc49fe82056367aee13b03c685d5c5b5f6103
manisha1896 <manisha.4918@gmail.com>
2019-01-03 22:15:45 +0530

    print X using digit

---
+++ Patterns/print_X_using_digit/index.java
@@ -0,0 +1,55 @@
+/**
+ @author: JsCodeLover
+ Print X using digits. (n=)
+  1     1
+   2   2 
+    3 3
+     4
+    3 3
+   2   2
+  1     1
+ */
+
+import java.util.*;
+class PrintX
+{
+	public static void main(String ar[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the no of row : ");
+		int num=sc.nextInt();
+		int spacebtn=1,spacebef=0,loopEnd,numPrint=1;
+		for(int i=3;i<=num;i++)
+			spacebtn+=2;
+		loopEnd=(num-1)*2+1;
+		for(int i=1;i<=loopEnd;i++)
+		{
+			for(int j=1;j<=spacebef;j++)
+				System.out.print(" ");
+			System.out.print(numPrint);
+			for(int k=1;k<=spacebtn;k++)
+				System.out.print(" ");
+			if(i!=num)
+			    System.out.print(numPrint);
+			if(i<num-1)
+			{
+				spacebtn-=2;
+				spacebef++;
+				numPrint++;
+			}
+			else if((i+1)==num)
+			{
+				spacebef++;
+				numPrint++;
+				spacebtn-=2;
+			}
+			else
+			{
+				spacebtn+=2;
+				spacebef--;
+				numPrint--;
+			}
+			System.out.println();
+		}	
+	}
+}
