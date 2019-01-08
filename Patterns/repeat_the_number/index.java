30c5b31363f3e0ee8410ece629381c8e873045ee
manisha1896 <manisha.4918@gmail.com>
2019-01-08 20:41:59 +0530

    repeat the number

---
+++ Patterns/repeat_the_number/index.java
@@ -0,0 +1,33 @@
+/**
+ @author: Repeat the Number. (n=4)
+    1
+    23
+    4567
+    891234567891234
+ */
+
+import java.util.*;
+class Tests
+{
+	public static void main(String ar[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the no of row : ");
+		int num=sc.nextInt();
+		int numprint=2, loopEnd=1;
+		System.out.println("1");
+		for(int i=2;i<=num;i++)
+		{
+			loopEnd*=2;
+			for(int j=1;j<=loopEnd;j++)
+			{
+				if(numprint==10)
+					numprint=1;
+				System.out.print(numprint++);
+			}	
+			System.out.println();
+		}	
+	}
+}
+
+ 
